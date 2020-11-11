1a9935443af5cd9566010ca4c91b5961992b05c7
balib01 <balib1437@gmail.com>
2020-11-11 20:13:40 +0400

    modified

--- menuBar/script.js
+++ menuBar/script.js
@@ -2,10 +2,10 @@ const hamburger = document.querySelector('#hamburger');
 const navUl =  document.querySelector('#nav-ul');
 
 hamburger.addEventListener('click', () => {
- 
-  if(hamburger.classList.contains(".hide")) {
-    hamburger.classList.remove(".hide");
+  console.log("clicked")
+  if(navUl.classList.contains("hide")) {
+    navUl.classList.remove("hide");
   } else {
-   hamburger.classList.add(".hide");
+   navUl.classList.add("hide");
   }
 });
