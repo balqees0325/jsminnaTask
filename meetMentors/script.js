d8b418691852617cc004df6f32cd50803d0564b1
balib01 <balib1437@gmail.com>
2020-11-26 15:31:01 +0400

    blog

--- meetMentors/script.js
+++ meetMentors/script.js
@@ -1,9 +1,17 @@
+
 let name = document.querySelector('#name').value;
 let email = document.querySelector('#email').value;
+let name = document.querySelector('#mentee_track').value;
+let email = document.querySelector('#mentee_email').value;
+
+let frontendMentor = [Abdulqudus, Abdull, Bello, Khadeejah,Aisha];
+let backendMentors = [Abdulkabeer, Yusuf, Hameed, Zaliha, Debby]
+let randomFront = Math.floor(Math.round() * frontendMentor.length);
+let randomBack = Math.floor(Math.round() * backendMentor.length);
 
 
  const validateForm = () => {
-    console.log('clicked');
+  
    if(name.length < 10 ) {
      alert('field can not be empty/ entry to small!');
      return false;
